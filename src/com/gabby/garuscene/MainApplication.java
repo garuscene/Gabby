f15bffd57f646f19156c3b3f4e3c09b172c55e85
garuscene <aganaluizgabriel08+github1@gmai.com>
2017-08-06 12:12:24 +0800

    Fixed layout

--- src/com/gabby/garuscene/MainApplication.java
+++ src/com/gabby/garuscene/MainApplication.java
@@ -6,6 +6,7 @@ import javax.swing.text.SimpleAttributeSet;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 
 public class MainApplication {
     public static void main(String[] args) {
@@ -14,21 +15,32 @@ public class MainApplication {
         frame.setSize(960, 540);
         frame.setLocationRelativeTo(null);
         frame.setResizable(false);
+        frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
 
-        //Text console
+        //Text console=================
         JTextPane console = new JTextPane();
-        console.setBounds(0, 0, frame.getWidth(), frame.getHeight() - 100);
-        //console.setEditable(false);
+        console.setEditable(false);
         console.setMargin(new Insets(10, 10, 10, 10));
+        console.setPreferredSize(new Dimension(960, 520));
 
         SimpleAttributeSet set = new SimpleAttributeSet();
         StyleConstants.setLineSpacing(set, 0.12f);
         StyleConstants.setFontFamily(set, "Consolas");
         console.setParagraphAttributes(set, true);
 
-        frame.getContentPane().add(console);
+        frame.add(console);
 
-        frame.setLayout(null);
+        //Input========================
+        JTextField input = new JTextField();
+        input.setMargin(new Insets(5, 5, 5, 5));
+        input.setFont(new Font("Consolas", Font.PLAIN, 12));
+
+        frame.add(input);
+
+        //End==========================
+        frame.pack();
         frame.setVisible(true);
+
+        input.requestFocus();
     }
 }
