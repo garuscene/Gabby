f1c941809f2eec6239173a984f9cf18ded32bd32
garuscene <aganaluizgabriel08+github1@gmai.com>
2017-08-06 12:36:46 +0800

    Initial search response

--- src/com/gabby/garuscene/MainApplication.java
+++ src/com/gabby/garuscene/MainApplication.java
@@ -7,6 +7,11 @@ import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.*;
 
 public class MainApplication {
     public static void main(String[] args) {
@@ -34,6 +39,12 @@ public class MainApplication {
         JTextField input = new JTextField();
         input.setMargin(new Insets(5, 5, 5, 5));
         input.setFont(new Font("Consolas", Font.PLAIN, 12));
+        input.addActionListener(new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                searchDictinonary(input.getText());
+            }
+        });
 
         frame.add(input);
 
@@ -42,5 +53,32 @@ public class MainApplication {
         frame.setVisible(true);
 
         input.requestFocus();
+    }
+
+    private static void searchDictinonary(String word) {
+        try {
+            URL url = new URL("http://gabbydictionary.com/");
+            URLConnection con = url.openConnection();
+            con.setDoOutput(true);
+
+            PrintWriter wr = new PrintWriter(con.getOutputStream(), true);
+
+            StringBuilder parameters = new StringBuilder();
+            parameters.append("q=" + URLEncoder.encode(word, "UTF-8"));
+            parameters.append("&");
+            parameters.append("x=" + URLEncoder.encode("0", "UTF-8"));
+            parameters.append("&");
+            parameters.append("y=" + URLEncoder.encode("0", "UTF-8"));
+            wr.println(parameters);
+            wr.close();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            String line;
+            while((line = br.readLine()) != null) System.out.println(line);
+            br.close();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
