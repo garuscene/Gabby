b56a07aa69044aaf056ffaaa7fa71f4dcdd29d48
garuscene <aganaluizgabriel08+github1@gmai.com>
2017-08-06 11:24:57 +0800

    Initial frame

---
+++ src/com/gabby/garuscene/MainApplication.java
@@ -0,0 +1,13 @@
+package com.gabby.garuscene;
+
+import javax.swing.*;
+
+public class MainApplication {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Gabby");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(960, 540);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
